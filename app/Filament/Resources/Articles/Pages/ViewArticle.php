6367caa383d516e5d057be537a8509eb21f5e2a9
Joaquim Perez <jivkim@gmail.com>
2025-06-25 16:35:42 +0200

    review article resource

--- app/Filament/Resources/Articles/Pages/ViewArticle.php
+++ app/Filament/Resources/Articles/Pages/ViewArticle.php
@@ -5,10 +5,21 @@ namespace App\Filament\Resources\Articles\Pages;
 use App\Filament\Resources\Articles\ArticleResource;
 use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Contracts\Support\Htmlable;
 
 class ViewArticle extends ViewRecord
 {
     protected static string $resource = ArticleResource::class;
+
+    public function getTitle(): string | Htmlable
+    {
+        return $this->record->title;
+    }
+
+    public function getSubheading(): ?string
+    {
+        return $this->record->ref;
+    }
 
     protected function getHeaderActions(): array
     {
