5813467f213f5061989e9bb418549d02f5fa6f84
Joaquim Perez <jivkim@gmail.com>
2025-10-10 15:52:42 +0200

    review fund infolist and form using sections

--- app/Filament/Resources/Funds/Schemas/FundForm.php
+++ app/Filament/Resources/Funds/Schemas/FundForm.php
@@ -6,6 +6,7 @@ use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class FundForm
@@ -14,26 +15,29 @@ class FundForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->label('Nom')
-                    ->required(),
-                TextInput::make('ref')
-                    ->label("Cote")
-                    ->required(),
-                Select::make('location_id')
-                    ->label('Localisation')
-                    ->relationship('location', 'name')
-                    ->searchable(['name'])
-                    ->preload(),
-                Textarea::make('description'),
-                DatePicker::make('creation_date')
-                    ->label("Date de création"),
-                TextInput::make('year_from')
-                    ->label("Années, de")
-                    ->numeric(),
-                TextInput::make('year_to')
-                    ->label("Années, à")
-                    ->numeric(),
-            ]);
+                Section::make('Classification')->schema([
+                    TextInput::make('ref')->label("Cote")->required(),
+                    TextInput::make('name')->label('Nom')->required(),
+
+                    Textarea::make('description')->columnSpan(2),
+
+                    Select::make('location_id')
+                        ->label('Localisation')
+                        ->relationship('location', 'name')
+                        ->searchable(['name'])
+                        ->preload(),
+
+                    DatePicker::make('creation_date')
+                        ->label("Date de création"),
+
+                    TextInput::make('year_from')
+                        ->label("Années, de")
+                        ->numeric(),
+                    TextInput::make('year_to')
+                        ->label("Années, à")
+                        ->numeric(),
+                ])->columns(2),
+
+            ])->columns(1);
     }
 }
--- app/Filament/Resources/Funds/Schemas/FundInfolist.php
+++ app/Filament/Resources/Funds/Schemas/FundInfolist.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Funds\Schemas;
 
 use App\Models\Fund;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class FundInfolist
@@ -12,18 +13,28 @@ class FundInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('description'),
-                TextEntry::make('location.name')
-                    ->label('Localisation')
-                    ->numeric(),
-                TextEntry::make('creation_date')
-                    ->label("Date de création")
-                    ->isoDate('LL'),
-                TextEntry::make('year_from')
-                    ->label("Années")
-                    ->formatStateUsing(fn(Fund $record) => "de $record->year_from à $record->year_to"),
-                TextEntry::make('created_at'),
-                TextEntry::make('updated_at'),
-            ]);
+                Section::make('Classification')->schema([
+                    TextEntry::make('ref')->label('Cote')->columnSpan(2),
+                    TextEntry::make('name')->columnSpan(2),
+
+                    TextEntry::make('description')->columnSpan(4),
+
+                    TextEntry::make('location.name')
+                        ->label('Lieu')
+                        ->numeric(),
+                    TextEntry::make('creation_date')
+                        ->label("Date de création")
+                        ->isoDate('LL'),
+                    TextEntry::make('year_from')
+                        ->label("Années")
+                        ->formatStateUsing(fn(Fund $record) => "de $record->year_from à $record->year_to"),
+                ])->columns(4),
+
+                Section::make('Système')->schema([
+                    TextEntry::make('id')->label("ID"),
+                    TextEntry::make('created_at'),
+                    TextEntry::make('updated_at'),
+                ])->columns(4)->collapsible()->persistCollapsed(),
+            ])->columns(1);
     }
 }
