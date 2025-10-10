3d3cb8848706fbbb21085efbfa46c96586402829
Joaquim Perez <jivkim@gmail.com>
2025-10-10 18:01:00 +0200

    Person: new name accessor

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
@@ -15,5 +16,12 @@ class Person extends Model
     public function articles(): BelongsToMany
     {
         return $this->belongsToMany(Article::class);
+    }
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => "$this->first_name $this->last_name"
+        );
     }
 }
