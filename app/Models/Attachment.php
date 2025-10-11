2f36e347f3c702fb9d717a4bceecfb227c02ced3
Joaquim Perez <jivkim@gmail.com>
2025-10-11 17:17:46 +0200

    feat(Attachment): store thumbnail

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Laravel\Facades\Image;
 
 class Attachment extends Model
 {
+    protected static function booted()
+    {
+        static::creating(function (Attachment $attachment) {
+            try {
+                $attachment->createThumbnail();
+            } catch (\Exception $e) {
+                Log::error("Error creating thumbnail for attachment $attachment->id", ['message' => $e->getMessage()]);
+            }
+        });
+    }
 
     protected function casts(): array
     {
@@ -29,5 +41,16 @@ class Attachment extends Model
     public function url(): string
     {
         return Storage::temporaryUrl($this->path, now()->addMinutes(30));
+    }
+
+    public function createThumbnail()
+    {
+        if (!$this->isImage()) {
+            return;
+        }
+        $img = Image::read(Storage::get($this->path));
+        $img->scaleDown(height: 222);
+        $this->thumbnail_path = "thumbnails/$this->path";
+        Storage::put($this->thumbnail_path, $img->encode());
     }
 }
