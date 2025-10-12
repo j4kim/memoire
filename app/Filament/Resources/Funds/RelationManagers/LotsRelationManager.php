16c8fc778326f2ca4fee58beec729f9a9c191c60
Joaquim Perez <jivkim@gmail.com>
2025-10-12 23:03:15 +0200

    Funds/LotsRelationManager: custom create action to attach new lot to fund

--- app/Filament/Resources/Funds/RelationManagers/LotsRelationManager.php
+++ app/Filament/Resources/Funds/RelationManagers/LotsRelationManager.php
@@ -2,13 +2,18 @@
 
 namespace App\Filament\Resources\Funds\RelationManagers;
 
+use App\Filament\Resources\Funds\Pages\EditFund;
 use App\Filament\Resources\Lots\LotResource;
+use App\Models\Lot;
+use Filament\Actions\Action;
 use Filament\Actions\AssociateAction;
-use Filament\Actions\CreateAction;
 use Filament\Actions\DissociateAction;
 use Filament\Actions\ViewAction;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Support\Enums\Width;
 use Filament\Tables\Table;
+use Livewire\Component;
 
 class LotsRelationManager extends RelationManager
 {
@@ -22,7 +27,27 @@ class LotsRelationManager extends RelationManager
         return $table
             ->headerActions([
                 AssociateAction::make(),
-                CreateAction::make(),
+                Action::make('create')
+                    ->label("Créer")
+                    ->schema([
+                        TextInput::make('name')->required(),
+                    ])
+                    ->modalWidth(Width::Large)
+                    ->modalHeading("Créer un lot dans ce fond")
+                    ->modalSubmitActionLabel('Créer')
+                    ->action(function (array $data, Component $livewire) {
+                        $fund = $this->getOwnerRecord();
+                        $lot = new Lot($data);
+                        $lot->fund_id = $fund->id;
+                        $lot->ref = uniqid('temp');
+                        $lot->save();
+                        $lot->ref = "$fund->ref/$lot->id";
+                        $lot->save();
+                        $fund->lots()->save($lot);
+                        $livewire->redirect(LotResource::getUrl('view', ['record' => $lot]));
+                    })->visible(function (Component $livewire) {
+                        return $livewire->pageClass === EditFund::class;
+                    }),
             ])
             ->recordActions([
                 DissociateAction::make(),
