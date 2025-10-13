9e6c728b43b6c7a35c3cd0d6c627c6d0abb4649f
Joaquim Perez <jivkim@gmail.com>
2025-10-13 17:39:35 +0200

    adapt title in article replicate action

--- app/Filament/Resources/Articles/Actions/Replicate.php
+++ app/Filament/Resources/Articles/Actions/Replicate.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\Articles\Actions;
 use App\Filament\Resources\Articles\ArticleResource;
 use App\Models\Article;
 use Filament\Actions\Action;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\TextInput;
+use Filament\Support\Enums\Width;
 use Filament\Support\Icons\Heroicon;
 use Livewire\Component;
 
@@ -21,13 +24,16 @@ class Replicate extends Action
         parent::setUp();
 
         $this->label("Dupliquer");
-
         $this->icon(Heroicon::Square2Stack);
+        $this->modalWidth(Width::Large);
 
-        $this->requiresConfirmation();
+        $this->schema([
+            TextInput::make('title')->default(fn(Article $record) => $record->title),
+        ]);
 
-        $this->action(function (Component $livewire, Article $record): void {
+        $this->action(function (array $data, Component $livewire, Article $record): void {
             $replica = $record->replicate();
+            $replica->fill($data);
             $replica->ref = $record->getNextRef();
             $replica->save();
             $livewire->redirect(
